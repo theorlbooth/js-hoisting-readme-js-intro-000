908c9139a4424be91ad66b2c76f8a4132e74dd11
Theo Booth <theorlbooth@googlemail.com>
2020-02-18 16:01:57 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -6,26 +6,3 @@ function callMe() {
   console.log("so call me");
   return lyric;
 }
-
-
-function crazy() {
-  // fix the code in here:
-  thisIsCrazy();
-​
-  var thisIsCrazy = function (){
-    console.log("hey!!!")
-  }
-}
-
-
-function sayMyName() {
-  // fix the code in here:
-  var name = "Cricky";
-
-  sayMy();
-
-  function sayMy() {
-    console.log(name);
-    var name = "Kristin";
-  }
-}
